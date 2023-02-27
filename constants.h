be4c7dc237b6bafa6b9548dc766949debf6c176b
wddzbcdr <wddzbcdr@gmail.com>
2023-02-26 23:49:31 -0800

    First Commit - Baicheng
    constant.h includes all defined costant values.
    BasicStructures.c includes part1, please check the comment for more details
    main.c includes test cases for my functions, feel free to modify as needed
    
    Signed-off-by: wddzbcdr <wddzbcdr@gmail.com>

---
+++ constants.h
@@ -0,0 +1,11 @@
+// Basic constants include
+#define PAGE_SIZE           1024        // 2^10
+#define PAGE_NUM            64          // 2^6
+#define FRAME_SIZE          1024        // 2^10
+#define FRAME_NUM           16          // 2^4
+#define BLOCK_SIZE          1024        // 2^10
+#define MAX_BLOCK           128         // 2^7
+
+// Error constants
+#define NO_FREE_BLOCK      -1
+#define NO_BLOCK_ERROR     -1
